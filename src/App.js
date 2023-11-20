aa5c0548f5c64f592ff829222d0014ae32276998
odfrank <oyedeledamilaref@yahoo.com>
2023-11-19 21:01:16 -0700

    Fixee the initial page rendering issues. Front end now works fine

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ width: "60%", margin: "auto" }}>
+    <h3>New Expense</h3>
+    <ExpenseForm />
+    <hr style={{ border: "1px solid grey" }} />
+    <h3>Your Expenses</h3>
+    <ExpenseList />
+  </div>
+);
 
 export default App;
---
+++ src/services/expenses.js
@@ -0,0 +1,49 @@
+import { ActionCreators } from "../app/expensesReducer";
+
+export const GetExpenses = async (dispatch) => {
+  try {
+    // api call
+    const expenses = [
+      { id: 1, description: "Groceries", amount: 23.16 },
+      { id: 2, description: "Gas", amount: 18.52 },
+      { id: 3, description: "Student Loans", amount: 600 },
+    ];
+
+    dispatch(ActionCreators.setExpenses(expenses));
+  } catch {
+    console.log("Error!");
+  }
+};
+
+export const NewExpense = async (dispatch, expense) => {
+  try {
+    // api call
+    dispatch(
+      ActionCreators.newExpense({
+        id: 10,
+        description: expense.description,
+        amount: expense.amount,
+      })
+    );
+  } catch {
+    console.log("Error!");
+  }
+};
+
+export const EditExpense = async (dispatch, expense) => {
+  try {
+    // api call
+    dispatch(ActionCreators.editExpense(expense));
+  } catch {
+    console.log("Error!");
+  }
+};
+
+export const DeleteExpense = async (dispatch, expense) => {
+  try {
+    // api call
+    dispatch(ActionCreators.deleteExpense(expense));
+  } catch {
+    console.log("Error!");
+  }
+};
